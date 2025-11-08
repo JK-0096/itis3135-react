b28ed0e9fe04bf005a2fb4e220bf386213bc3c5f
clorxv <clorxv@gmail.com>
2025-11-08 15:36:58 -0500

    react

--- itis3135-react/src/Header.jsx
+++ itis3135-react/src/Header.jsx
@@ -9,10 +9,10 @@ export default function Header() {
                 <NavLink to="/Contract" end className="nav-link">Contract</NavLink>
             </nav>
             <nav>
-            <a href="ITIS3135/stuff/ROCK&YOU.htm">Crappy Webpage Assignment</a>
-            <a href="ITIS3135/hobby/index.html">Hobby Webpage Assignment</a>
-            <a href="ITIS3135/intro_form.html">Intro Form</a>
-            <a href="ITIS3135/fccfsjs_outline.html">FCC Full Stack JS Outline</a>
+            <a href="ITIS3135/stuff/ROCK&YOU.htm">Crappy Webpage Assignment || </a>
+            <a href="ITIS3135/hobby/index.html">Hobby Webpage Assignment || </a>
+            <a href="ITIS3135/intro_form.html">Intro Form || </a>
+            <a href="ITIS3135/fccfsjs_outline.html">FCC Full Stack JS Outline || </a>
             <a href="ITIS3135/project/index.html">JavaScript Learning Site Project</a>
             </nav>
         </header>

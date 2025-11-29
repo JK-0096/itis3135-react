1add347b7c93aea3a97abd07d61580ca1b4ed637
clorxv <clorxv@gmail.com>
2025-11-29 15:01:22 -0500

    a

--- itis3135-react/src/Students.jsx
+++ itis3135-react/src/Students.jsx
@@ -17,12 +17,12 @@ export default function Students() {
                         <h2>{student.name.preferred || student.name.first} {student.name.last}</h2>
                         <p><strong>Prefix:</strong> {student.prefix}</p>
 
-                        <p><strong>Acknowledgement:</strong> {student.acknowledgement}</p>
+                        {/* <p><strong>Acknowledgement:</strong> {student.acknowledgement}</p>
                         <p><strong>Mascot:</strong> {student.mascot}</p>
 
                         <h3>Platform</h3>
                         <p><strong>Device:</strong> {student.platform.device}</p>
-                        <p><strong>OS:</strong> {student.platform.os}</p>
+                        <p><strong>OS:</strong> {student.platform.os}</p> */}
 
                         {/* <h3>Backgrounds</h3>
                         <p><strong>Personal:</strong> {student.backgrounds.personal}</p>

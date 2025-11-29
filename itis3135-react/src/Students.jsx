5a657362007ed61b1db3d1e68de1dec4814a6e48
clorxv <clorxv@gmail.com>
2025-11-29 15:12:05 -0500

    s

--- itis3135-react/src/Students.jsx
+++ itis3135-react/src/Students.jsx
@@ -65,7 +65,6 @@ export default function Students() {
                             )
                         } */}
 
-                        {/* hello */}
                         
 
                     </div>
